269835d489ee9de893e203d2044b4957e874ae71
Mounika2512 <mounikasaviti25@gmail.com>
2024-03-02 10:44:04 +0530

    GetMax

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -1,12 +1,23 @@
 public class GetMax {
     /**
-     * This method should return the max value of arr (the largest int.)
-     * This problem will require you to use a for loop. No other guidance will be provided.
+     * Returns the maximum value in the given array.
      *
-     * @param arr an array of ints to be manipulated.
+     * @param arr an array of integers.
      * @return the largest value in arr.
      */
-    public int max(int[] arr){
-        return -1;
+    public int max(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Array must not be empty or null");
+        }
+
+        int max = arr[0]; // Assume the first element is the maximum initially.
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i]; // Update max if a larger value is found.
+            }
+        }
+
+        return max;
     }
 }
